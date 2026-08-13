67017cdf98958a5249d5fdb46db11c73ef70f9a6
achirav <achirav22@gmail.com>
2026-08-13 23:29:10 +0530

    testing commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,8 @@ public class Main {
   public static void main(String[] args){
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
+      System.out.println("asasd");
+
 
     //  Uncomment the code below to pass the first stage
         ServerSocket serverSocket = null;
